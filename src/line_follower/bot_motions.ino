6a198c2098dc030f19d4e8bb2528ed56969881d3
briancliau <brian.liau@tufts.edu>
2025-09-24 17:20:46 -0400

    Add files via upload

--- src/line_follower/bot_motions.ino
+++ src/line_follower/bot_motions.ino
@@ -12,10 +12,10 @@ void setup() {
   pinMode(motor2_in1, OUTPUT);
   pinMode(motor2_in2, OUTPUT);
   pinMode(motor1_en12_pin, OUTPUT);
-  pinMode(motor1_en34_pin, OUTPUT);
+  pinMode(motor2_en34_pin, OUTPUT);
 }
 
-void bot_stop(int speed) {
+void bot_stop() {
   // Motor 1 
   analogWrite(motor1_in1, 0);
   analogWrite(motor1_in2, 0);
@@ -24,7 +24,7 @@ void bot_stop(int speed) {
   analogWrite(motor2_in2, 0);
 }
 
-void bot_forward()) {
+void bot_forward(int speed) {
   // Motor 1 
   analogWrite(motor1_in1, speed);
   analogWrite(motor1_in2, 0);
@@ -91,7 +91,30 @@ void bot_turnLeft(int turn_radius) {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(motor1_en12_pin, HIGH); // turns on enable for motor 1
-  digitalWrite(motor1_en12_pin, HIGH); // turns on enable for motor 2
+  digitalWrite(motor2_en34_pin, HIGH); // turns on enable for motor 2
 
   // Call other functions to move bot
+  bot_forward(200);
+  delay(2000);
+  bot_stop();
+  delay(2000);
+  bot_backwards(200);
+  delay(2000);
+  bot_stop();
+  delay(2000);
+  bot_clockwise();
+  delay(2000);
+  bot_stop();
+  delay(2000);
+  bot_counterclockwise();
+  delay(2000);
+  bot_stop();
+  delay(2000);
+  bot_turnRight(10);
+  delay(2000);
+  bot_stop();
+  delay(2000);
+  bot_turnLeft(10);
+  delay(2000);
+  bot_stop();
 }
